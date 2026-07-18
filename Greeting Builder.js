2246f6fd567de4e47c9209d88fa66f86360833af
tonymtta <tonymtta@gmail.com>
2026-07-18 19:28:46 +1000

    Add files via upload

---
+++ Greeting Builder.js
@@ -0,0 +1,17 @@
+function getGreeting(timeOfDay) {
+  if (timeOfDay === "morning") {
+    return "Good morning!";
+  } else if (timeOfDay === "afternoon") {
+    return "Good afternoon!";
+  } else if (timeOfDay === "evening") {
+    return "Good evening!";
+  }
+}
+
+function formatName(firstName, lastName) {
+  return `${firstName} ${lastName}`;
+}
+
+function createGreeting(firstName, lastName, timeOfDay) {
+    
+}
